e86c7171d4c0bc450f7af61126c3868169ef1e23
CGNjea <aryeejodi@gmail.com>
2018-06-19 20:12:25 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizz_buzz(int)
+  if int%15 == 0
+    puts "FizzBuzz"
+  elsif int%3 == 0
+    puts "Fizz"
+  elsif int%5 == 0
+    puts "Buzz"
+  else
+    puts "nil"
+   end
+end
+
+fizz_buzz(3)
+fizz_buzz(5)
+fizz_buzz(15)
+fizz_buzz(14)
